d6ded219ae696dc4112ab4cc715ec4179f5ed9b1
raunakyadav777 <raunakyadav777@gmail.com>
2021-10-12 23:20:44 +0530

    Mail

--- user/helper.php
+++ user/helper.php
@@ -3,55 +3,39 @@ include("user.php");
 $ob = new User();
 extract($_POST);
 if ($action == 'insert') {
-    $order_id="1001";
-    $image_err = false;
-    function fileupload($filename)
-    {
-        if (isset($_FILES[$filename]['name'])) {
-            $file = $_FILES[$filename]['name'];
-            $tmp_name = $_FILES[$filename]['tmp_name'];
-            $ext = pathinfo($file);
-            $valid_ext = array('jpg','jpeg','png');
-            if (in_array($ext['extension'], $valid_ext)) {
-                $new_file_name = rand().".".$ext['extension'];
-                $path = "../images/".$new_file_name;
-                if (move_uploaded_file($tmp_name, $path)) {
-                    echo "Upload Successfully";
-                    $GLOBALS['image_err'] = true;
-                }
-                else{
-                    $GLOBALS['image_err'] = false;
-                }
+    $order_id = "1001";
+    if ($_FILES['logofile']['name'] || $_FILES['samplefile']['name']) {
+        $location = "../images/";
+        $file1 = $_FILES['logofile']['name'];
+        $tmp_name1 = $_FILES['logofile']['tmp_name'];
+        $file2 = $_FILES['samplefile']['name'];
+        $tmp_name2 = $_FILES['samplefile']['tmp_name'];
+        $ext1 = pathinfo($file1);
+        $ext2 = pathinfo($file2);
+        $valid_ext = array('jpg', 'jpeg', 'png','pdf');
+        if (in_array($ext1['extension'], $valid_ext) && in_array($ext2['extension'], $valid_ext)) {
+            if ($term == 'on') {
+                $term = true;
+            } else {
+                $term = false;
             }
-            else{
-                $GLOBALS['image_err'] = false;
+            $flag = $ob->insertdata($order_id, $description, $product_number, $qauntity, $date, $logos, $file1, $department, $site, $projectowner, $file2, $phone, $InputEmail, $term);
+            if ($flag) {
+                move_uploaded_file($tmp_name1, $location . $file1);
+                move_uploaded_file($tmp_name2, $location . $file2);
             }
+            echo "Upload";
+        } else {
+            echo "File not valid";
         }
     }
-   
-    $filename = array("logofile", "samplefile");
-    $key = array_keys($_FILES);
-    for($i = 0; $i< count($filename); $i++)
-    {
-        if($filename[$i] == $key[$i])
-        {
-            fileupload($filename[$i]);
+    else{
+        if ($term == 'on') {
+            $term = true;
+        } else {
+            $term = false;
         }
-        print_r($image_err);
+        $flag = $ob->insertdata($order_id, $description, $product_number, $qauntity, $date, $logos, $file1, $department, $site, $projectowner, $file2, $phone, $InputEmail, $term);
+        echo $flag;
     }
-
-   
-
-
-    // if($term == 'on')
-    // {
-    //     $term = true;
-    // }
-    // else{
-    //     $term = false;
-    // }
-    // $flag=$ob->insertdata($order_id, $description,$product_number,$qauntity,$date,$logos,$logo_file,$department,$site,$projectowner,$sample_file,$phone,$InputEmail,$term);
-    // echo $flag;
 }
-
-
