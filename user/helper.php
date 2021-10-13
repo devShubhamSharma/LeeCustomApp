10153faa517b3111c7549680a237c3a8003d7da0
Your Name <akbharti1717@gmail.com>
2021-10-13 09:17:10 +0530

    Final commit after upload issue resolved

--- user/helper.php
+++ user/helper.php
@@ -4,54 +4,111 @@ $ob = new User();
 extract($_POST);
 if ($action == 'insert') {
     $order_id="1001";
-    $image_err = false;
-    function fileupload($filename)
+      
+    $location="../images/";
+    $uploadflags=array($_FILES['logofile']['error'],$_FILES['samplefile']['error']);
+   
+    $file1=$_FILES['logofile']['name'];
+    $file_tmp1=$_FILES['logofile']['tmp_name'];
+    $file2=$_FILES['samplefile']['name'];
+    $file_tmp2=$_FILES['samplefile']['tmp_name'];
+    
+    if($term == 'on')
     {
-        if (isset($_FILES[$filename]['name'])) {
-            $file = $_FILES[$filename]['name'];
-            $tmp_name = $_FILES[$filename]['tmp_name'];
-            $ext = pathinfo($file);
-            $valid_ext = array('jpg','jpeg','png');
-            if (in_array($ext['extension'], $valid_ext)) {
-                $new_file_name = rand().".".$ext['extension'];
-                $path = "../images/".$new_file_name;
-                if (move_uploaded_file($tmp_name, $path)) {
-                    echo "Upload Successfully";
-                    $GLOBALS['image_err'] = true;
-                }
-                else{
-                    $GLOBALS['image_err'] = false;
-                }
+        $term = true;
+    }
+    else{
+        $term = false;
+    }
+    $ext1 = pathinfo($file1);
+    $ext2 = pathinfo($file2);
+    $len=count($_FILES);
+    print_r($_FILES['logofile']['error']);
+    $valid_ext = array('jpg','jpeg','png');
+    print_r($uploadflags);
+    if($uploadflags[0]==0 && $uploadflags[1]==0 ){
+        echo "Bothfile";
+       
+        if (in_array($ext1['extension'], $valid_ext) && in_array($ext2['extension'], $valid_ext)) {
+            $flag=$ob->insertdata($order_id, $description,$product_number,$qauntity,$date,$logos,$file1,$department,$site,$projectowner,$file2,$phone,$InputEmail,$term);
+            if($flag && $term == true)
+            {
+                move_uploaded_file($file_tmp1, $location.$file1);
+                move_uploaded_file($file_tmp2, $location.$file2);
+                echo "success";
             }
-            else{
-                $GLOBALS['image_err'] = false;
+            else
+            {
+                echo "failed";
             }
+            
+        }
+        else{
+            echo "Invalid Extension";
+            
+        }
+
+    }else if($uploadflags[0]==0 && $uploadflags[1]==4 ){
+        echo "logo";
+        if (in_array($ext1['extension'], $valid_ext)) {
+            $flag=$ob->insertdata($order_id, $description,$product_number,$qauntity,$date,$logos,$file1,$department,$site,$projectowner,$file2,$phone,$InputEmail,$term);
+            if($flag && $term == true)
+            {
+                move_uploaded_file($file_tmp1, $location.$file1);
+                echo "success";
+            }
+            else
+            {
+                echo "failed";
+            }
+            
+        }
+        else{
+            echo "Invalid Extension";
+            
+        }
+    }elseif($uploadflags[0]==4 && $uploadflags[1]==0){
+        echo "sample";
+        if (in_array($ext2['extension'], $valid_ext)) {
+            $flag=$ob->insertdata($order_id, $description,$product_number,$qauntity,$date,$logos,$file1,$department,$site,$projectowner,$file2,$phone,$InputEmail,$term);
+            if($flag && $term == true)
+            {
+                move_uploaded_file($file_tmp2, $location.$file2);
+                echo "success";
+            }
+            else
+            {
+                echo "failed";
+            }
+            
+        }
+        else{
+            echo "Invalid Extension";
+            
+        }
+    }else{
+        echo "insert record";
+        $logo_file="";
+        $sample_file="";
+        if($term==true){
+         $flag=$ob->insertdata($order_id, $description,$product_number,$qauntity,$date,$logos,$logo_file,$department,$site,$projectowner,$sample_file,$phone,$InputEmail,$term); 
+         if($flag){
+             echo "success";
+         }else{
+             echo "failed";
+         }  
+        }else{
+            echo "failed to insert";
         }
     }
-   
-    $filename = array("logofile", "samplefile");
-    $key = array_keys($_FILES);
-    for($i = 0; $i< count($filename); $i++)
-    {
-        if($filename[$i] == $key[$i])
-        {
-            fileupload($filename[$i]);
-        }
-        print_r($image_err);
-    }
+            
+}
+    
+	
+     
 
    
 
 
-    // if($term == 'on')
-    // {
-    //     $term = true;
-    // }
-    // else{
-    //     $term = false;
-    // }
     // $flag=$ob->insertdata($order_id, $description,$product_number,$qauntity,$date,$logos,$logo_file,$department,$site,$projectowner,$sample_file,$phone,$InputEmail,$term);
     // echo $flag;
-}
-
-
